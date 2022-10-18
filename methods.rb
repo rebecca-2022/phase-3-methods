9a618a994484edc056905269a8046c5a262ca17a
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-18 10:15:33 +0300

    complete assignment

--- methods.rb
+++ methods.rb
@@ -1,32 +1,32 @@
 # Your code here!
-def greet_programmer()
+def greet_programmer
     puts "Hello, programmer!"
 end
 
-
-def greet(name="Rebecca")
+def greet name
     puts "Hello, #{name}!"
-end
 
+end  
+greet ("Naureen")
+    greet ("Jimmy")  
 
-def greet_with_default(name="Rebecca")
-    puts"Hello, #{name}!"
-    puts"Hello, programmer!"
-end
+    def greet_with_default name="programmer"
+        puts "Hello, #{name}!"
+    end    
+    greet ("Naureen") 
+    greet_with_default()
 
-def add(num1, num2)
-    return num1 + num2
-end
-
-def halve (num1)
-    if num1.class != Integer
-        return nil
+    def add(num1, num2)
+         return num1+num2
     end
-    num1/2
+
+    def halve number
+    if number ==0 || number.class == String
+    p nil
+    return nil
+    end
+    puts number/2
+    return number/2
+    halve(4)
+    halve("h")
 end
-
-
-
-    
-
-
